2aac0e511c23ec530d3bd8260a38ec6218f3532f
Obi Marcus Onuora <obimarcus11@gmail.com>
2018-07-09 23:35:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays()
     return "Happy holidays!"
   }
   
-  function happyHoldaysTo(name)
+  function happyHolidaysTo(name)
     {
       return `Happy holidays, ${name}!`
     }
